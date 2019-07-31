846ca9b630c3631c0a85f6f2c65e8402cc3f7b8e
Carlos Vladimir Esmeralda Ponce <carlosv@Carloss-iMac.local>
2019-07-31 18:22:46 -0400

    nuevo campo de cantidad y nuevo componente tabla agregado

--- component.js
+++ component.js
@@ -5,12 +5,14 @@ var data={
             cantidad: 0
         }
     ],
-    nuevaFruta:""
+    nuevaFruta:"",
+    nuevaCant:0
 }
 Vue.component('ingresar',{
     template:`
         <div>
             <input type="text" v-model="nuevaFruta">
+            <input type="number" v-model="nuevaCant">
             <button @click="agregar">agregar</button>
         </div>
     `,
@@ -21,12 +23,28 @@ Vue.component('ingresar',{
         agregar(){
             this.frutas.push({
                 nombre: this.nuevaFruta,
-                cantidad: 0
+                cantidad: this.nuevaCant
             })
             this.nuevaFruta=''
+            this.nuevaCant=0
+            console.log(this.frutas)
         }
     }
 })
+Vue.component(
+    'tabla',{
+        template:`
+        <ul>
+            <li v-for="item in frutas">
+                {{item.nombre}} - {{item.cantidad}}
+            </li>
+        </ul>
+        `,
+        data(){
+            return data
+        }
+    }
+)
 var app=new Vue({
     el:"#app",
     data: data
